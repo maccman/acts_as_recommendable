144f8a11480080fd2aeb969a52a85624ad8ef95a
Alex MacCaw <maccman@gmail.com>
2008-09-05 13:03:17 +0100

    Even more C optimizations

--- lib/optimizations.rb
+++ lib/optimizations.rb
@@ -6,7 +6,28 @@ module MadeByMany
         inline do |builder|
           builder.c '
           #include <math.h>
-          double c_sim_pearson(double sum1, double sum2, double sum1Sq, double sum2Sq, double pSum, double n) {
+          double c_sim_pearson(VALUE si, VALUE prefs1, VALUE prefs2, int n) {
+            double sum1 = 0.0;
+            double sum2 = 0.0;
+            double sum1Sq = 0.0;
+            double sum2Sq = 0.0;
+            double pSum = 0.0;
+            
+            VALUE *prefs1_a = RARRAY(prefs1)->ptr;
+            VALUE *prefs2_a = RARRAY(prefs2)->ptr;
+            
+            int i;
+            for(i=0; i<n; i++) {
+              double prefs1_item = NUM2DBL(prefs1_a[i]);
+              double prefs2_item = NUM2DBL(prefs2_a[i]);
+                            
+              sum1   += prefs1_item;
+              sum2   += prefs2_item;
+              sum1Sq += pow(prefs1_item, 2);
+              sum2Sq += pow(prefs2_item, 2);
+              pSum   += prefs2_item * prefs1_item;
+            }
+            
             double num;
             double den;
             num = pSum - ( ( sum1 * sum2 ) / n );
@@ -28,21 +49,12 @@ module MadeByMany
       # Pearson score
       def self.sim_pearson(prefs, person1, person2)
         si = mutual_items(prefs[person1], prefs[person2])
+        
         n = si.length
 
         return 0 if n == 0
 
-        sum1 = sum2 = sum1Sq = sum2Sq = pSum = 0.0
-
-        si.each do |item|
-          sum1   += prefs[person1][item]
-          sum2   += prefs[person2][item]
-          sum1Sq += prefs[person1][item] ** 2
-          sum2Sq += prefs[person2][item] ** 2
-          pSum   += prefs[person2][item] * prefs[person1][item]
-        end
-
-        Optimizations.c_sim_pearson(sum1, sum2, sum1Sq, sum2Sq, pSum, n)
+        Optimizations.c_sim_pearson(si, prefs[person1].values, prefs[person2].values, n)
       end
 
     end     
