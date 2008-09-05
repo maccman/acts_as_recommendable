0978efad054b9b9631d156339031d4345c2be5f1
Alex MacCaw <maccman@gmail.com>
2008-09-05 11:00:05 +0100

    Add Inline C Optimizations

--- init.rb
+++ init.rb
@@ -1,3 +1,8 @@
 # Include hook code here
 require File.dirname(__FILE__) + '/lib/acts_as_recommendable'
 ActiveRecord::Base.send(:include, MadeByMany::ActsAsRecommendable)
+
+begin
+  require 'inline'
+  require File.dirname(__FILE__) + '/lib/optimizations'
+rescue LoadError; end
