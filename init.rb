c94a2695c98644fa029ff32a71f053a2f0870539
Alex MacCaw <maccman@gmail.com>
2008-12-09 17:41:51 +0000

    Fix RubyInline's permission problem

--- init.rb
+++ init.rb
@@ -6,6 +6,10 @@ require File.dirname(__FILE__) + '/lib/progress_bar'
 
 require File.dirname(__FILE__) + '/lib/cache_fix'
 
+# Fix RubyInline's permission problem,
+# RubyInline doesn't like directories with
+# group write permissions (like /tmp).
+ENV['INLINEDIR'] = File.join(Rails.root, 'tmp', 'rubyinline')
 begin
   require 'inline'
   require File.dirname(__FILE__) + '/lib/optimizations'
